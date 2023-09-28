5fd82ac9d78090d3680e1ef0c81241bd78f3be66
25619 <143705316+25619@users.noreply.github.com>
2023-09-28 12:55:27 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,31 @@
 import pyttsx3
 engine = pyttsx3.init()
-preset = []
+preset = {}
 
+def config():
+  content = ""
+  dicty = ""
+  with open("presets.txt", "r") as f:
+    content = f.read().split("-----")
+    for i in content:
+      if i != "<EOF>":
+        dicty = i.split("\n")
+        preset[dicty[0]] = dicty[1]
+        dicty = ""
+      else:
+        break
+counter = 1
+setting = input("Play preset? Y/N")
 while True:
-  engine.say(input("type statement here: "))
-  engine.runAndWait()
-
+  if setting == "Y":
+    print("Which preset to play?")
+    for i in preset:
+      print(f"{counter}. {i}")
+      counter += 1
+    engine.say(preset[input("I wanna play number ")[0]])
+    engine.runAndWait()
+    input("Press ENTER to end...")
+    break
+  else:
+    engine.say(input("type statement here: "))
+    engine.runAndWait()
